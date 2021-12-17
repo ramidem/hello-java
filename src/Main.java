fd7fc705f34a0f0e6451362e006b7813bbb15b35
Med Aduh <im.ramidem@gmail.com>
2021-12-18 03:03:31 +0800

    concatenating strings in java

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, student!");
 
-        int studentAge = 15;
+        int studentAge = 31;
         double studentGPA = 3.45;
         boolean hasPerfectAttendance = true;
 
         String studentFirstName = "Med";
         String studentLastName = "Aduh";
+        String studentFullName = studentFirstName + " " + studentLastName;
         char studentFirstInitial = studentFirstName.charAt(0);
         char studentLastInitial = studentLastName.charAt(0);
 
-        System.out.println(studentAge);
-        System.out.println(studentGPA);
-        System.out.println(hasPerfectAttendance);
-
-        System.out.println(studentFirstName);
-        System.out.println(studentLastName);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
+        System.out.println("Name: " + studentFullName);
+        System.out.println("Initials: " + studentFirstInitial + "." + studentLastInitial + ".");
+        System.out.println("Age: " + studentAge);
+        System.out.println("GPA: " + studentGPA);
+        System.out.println("Attendance: " + hasPerfectAttendance);
     }
 }
