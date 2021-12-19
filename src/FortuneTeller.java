b4236952c39443bddc9067e8c579ce3822759a29
Med Aduh <im.ramidem@gmail.com>
2021-12-19 17:40:29 +0800

    while loop

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -4,15 +4,26 @@ import java.util.Scanner;
 
 public class FortuneTeller {
     public static void main(String[] args) {
-        System.out.println("Please input a number between 1 and 10");
+        String prompt = "Please input a number between 1 and 10";
 
         Scanner scanner = new Scanner(System.in);
-        int pickedNumber = scanner.nextInt();
+        boolean isOpen = true;
 
-        if (pickedNumber < 5) {
-            System.out.println("Enjoy the good luck a friend brings you.");
-        } else {
-            System.out.println("Your shoe selection will make you happy today.");
+        while (isOpen) {
+            System.out.println(prompt);
+            int pickedNumber = scanner.nextInt();
+
+            if (pickedNumber == 0) {
+                isOpen = false;
+            } else {
+                if (pickedNumber < 5) {
+                    System.out.println("Enjoy the good luck a friend brings you.");
+                } else {
+                    System.out.println("Your shoe selection will make you happy today.");
+                }
+            }
         }
+
+        System.out.println("Exiting program.");
     }
 }
