15b68ba11b2584c85987584efcbc37c80df69f88
Med Aduh <im.ramidem@gmail.com>
2021-12-19 19:52:35 +0800

    parameters and return types

--- src/CalculateMealPrice.java
+++ src/CalculateMealPrice.java
@@ -1,21 +1,30 @@
 package src;
 
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class CalculateMealPrice {
-    public static void calculateMealPrice(
+    public static double calculateMealPrice(
         double listedMealPrice,
         double tipRate,
         double taxRate
     ) {
         double tip = tipRate * listedMealPrice;
         double tax = taxRate * listedMealPrice;
-        double total = listedMealPrice + tip + tax;
+        return listedMealPrice + tip + tax;
+    }
 
-        System.out.println("Your total meal price is: " + total);
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+
+    public static double getIndividualPayment(double total, int people) {
+        return total / people;
     }
 
     public static void main(String[] args) {
-        calculateMealPrice(15, .2, .08);
+        double totalMealPrice = calculateMealPrice(530, .2, .08);
+        double splitPayment = getIndividualPayment(totalMealPrice, 3);
+        
+        System.out.println("Total Meal Price: " + totalMealPrice);
+        System.out.println("Individual payment: " + df.format(splitPayment));
     }
 }
